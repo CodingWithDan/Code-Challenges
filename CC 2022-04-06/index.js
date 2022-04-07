5d4d6fd6d5e5c0ec0a494a948273d150d9e32c76
Daniel <codingwdan@gmail.com>
2022-04-06 22:21:56 -0700

    Add coding challenge for 04-06-2022.

--- CC 2022-04-06/index.js
+++ CC 2022-04-06/index.js
@@ -1,56 +1,13 @@
-//Title: Hello Object
-//Write the code, one line for each action:
 
-// Create an empty object user.
-// Add the property name with the value John.
-// Add the property surname with the value Smith.
-// Change the value of the name to Pete.
-// Remove the property name from the object.
+//Reverse List Order
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
 
-let user = {
-    
-}
-user.name = "John",
-user.surname ="Smith"
-user.name = "Pete";
-delete(user.name);
+// Examples (Input -> Output)
 
-console.log(user.surname)
-console.log(user.name)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+// * [9, 2, 0, 7]  -> [7, 0, 2, 9]
 
-
-// Title: Check for Emptiness
-// Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
-
-// Should work like that:
-
-let schedule = {};
-
-alert( isEmpty(schedule) ); // true
-
-schedule["8:30"] = "get up";
-
-alert( isEmpty(schedule) ); // false
-
-
-
-//Title: Sum object properties
-
-
-// We have an object storing salaries of our team:
-
-// Write the code to sum all salaries and store in the variable sum. Should be 390 in the example below.
-
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
+function reverseList(list) {
+  return list.reverse()
   }
-
-// If salaries is empty, then the result must be 0.
-
-let salaries = {
-    John: 100,
-    Ann: 160,
-    Pete: 130
-  }
+  
