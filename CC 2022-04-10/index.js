ba71c87786c2a34bc5f24e997f7338895325e6f7
Daniel <codingwdan@gmail.com>
2022-04-10 09:05:18 -0700

    Add coding challenge for 04-10-2022.

---
+++ CC 2022-04-10/index.js
@@ -0,0 +1,10 @@
+//=====================
+//Hex to Decimal
+//======================
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+function hexToDec(hexString){
+    //your code here
+  let yourNumber = parseInt(hexString, 16);
+    return yourNumber
+    }
