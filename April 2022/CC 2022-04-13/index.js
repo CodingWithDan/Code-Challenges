715a9da77e2d78386131fcbfac08e43914b86378
Daniel <codingwdan@gmail.com>
2022-04-13 21:13:22 -0700

    Add coding challenge for 04-13-2022.

---
+++ April 2022/CC 2022-04-13/index.js
@@ -0,0 +1,45 @@
+//===================================
+//Lario and Muigi Pipe Problem
+//===================================
+// Issue
+
+// Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+// The pipes connecting your level's stages together need to be fixed before you receive any more complaints. 
+// Each pipe should be connecting, since the levels ascend, you can assume every number in the sequence after the first index will be greater than the previous and that there will be no duplicates.
+// Task
+
+// Given the a list of numbers, return the list so that the values increment by 1 for each index up to the maximum value. 
+
+
+
+function pipeFix(numbers){
+    let output = []
+  for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++){
+    output.push(i)
+    
+  }
+    return output
+  }
+  
+  
+  //=================================
+  //What is betwen
+  //==================================
+  //Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+  
+  // For example:
+  
+  // a = 1
+  // b = 4
+  // --> [1, 2, 3, 4]
+  
+  function between(a, b) {
+    let output = [];
+    for (let i = a; i <= b; i++){
+      output.push(i)
+      
+    }
+    return output;
+  }
+  
