f65dfa32ee1ba3311e40b00ede5e14d80554561f
Daniel <codingwdan@gmail.com>
2022-07-13 19:39:40 -0700

    Add coding challenge for 7-13-2022 but with the code

--- July 2022/CC 2022-07-13/index.js
+++ July 2022/CC 2022-07-13/index.js
@@ -17,3 +17,12 @@
 // * Input: [1,2,3,4,5], output= [2,3,4,5]
 // * Input: [5,3,2,1,4], output = [5,3,2,4]
 // * Input: [2,2,1,2,1], output = [2,2,2,1]
+
+function removeSmallest(numbers) {
+    let newArr = numbers.slice(0)
+    
+    let smallestNum = numbers.indexOf(Math.min(...numbers))
+    newArr.splice(smallestNum, 1)
+      return newArr
+    }
+    
