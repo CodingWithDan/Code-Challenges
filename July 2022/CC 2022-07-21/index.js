9c2fd3831e07f1b1664061178c5573d4ef90ad9b
Daniel <codingwdan@gmail.com>
2022-07-21 15:47:40 -0700

    Added coding challenge for 07-21-2022

---
+++ July 2022/CC 2022-07-21/index.js
@@ -0,0 +1,21 @@
+
+
+//================================================
+//Enumerable Magic #3 - Does My List Include This?
+//================================================
+//Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+function include(arr, item){
+    if(arr.includes(item)){
+      return true
+    }
+    else{
+    return false}
+  }
+  
+  //OR
+  
+  function include(arr, item){
+    return arr.includes(item)
+  
+  }  
