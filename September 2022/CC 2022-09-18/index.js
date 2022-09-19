07cb02e1da718e1269c4a7142f8b18c6d882dbfb
Daniel <codingwdan@gmail.com>
2022-09-18 23:53:36 -0700

    Added coding challenge for September 18th, 2022

---
+++ September 2022/CC 2022-09-18/index.js
@@ -0,0 +1,15 @@
+//=================================
+//JavaScript.info -- FilterRange
+//=================================
+// Write a function filterRange(arr, a, b) that gets an array arr, looks for elements with values higher or equal to a and lower or equal to b and return a result as an array.
+
+// The function should not modify the array. It should return the new array.
+
+function filterRange(arr, a, b){
+    let dope =[]
+    let order = arr.sort()
+    arr.forEach
+}
+
+let arrd = [5, 3, 8, 1];
+filterRange(arrd)
