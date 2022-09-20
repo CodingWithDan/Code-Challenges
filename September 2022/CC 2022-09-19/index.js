b178a9033c7930dd10c453db1b5825fb29bdffbf
Daniel <codingwdan@gmail.com>
2022-09-19 19:11:27 -0700

    Added coding challenge for September 19th, 2022

--- September 2022/CC 2022-09-19/index.js
+++ September 2022/CC 2022-09-19/index.js
@@ -6,18 +6,37 @@
 //E: let arr = [5, 3, 8, 1];
 //  filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
 //  alert( arr ); // [3, 1]
+//P: Create a for... of loop. Each element will be referenced as the variable ele. 
+//P: to track the index, we'll create a variable called inx = arr.indexOf(ele)
+//P: we create an else statement, and excise that element at that index with splice if it violates it.
 
 function filterRangeInPlace(arr, a, b){
-    arr.forEach(x => {
-        if(x >= a && x <= b){
-            return x
+
+    for(const ele of arr){
+        let inx = arr.indexOf(ele)
+
+        if(ele < a || ele > b){
+            arr.splice(inx, 1)
         }
-        else{
-            arr.splice(arr[x], 1)
-        }
-    })
+    }
 }
 
 let arr = [5, 3, 8, 1]
+filterRangeInPlace(arr, 1, 4)
+console.log(arr)
+
+
+//We can also do this with a for...in loop, which instead interates over the index instead of the element
+
+function filterRangeInPlace(carr, a, b){
+
+    for(const i in carr){
+           if(carr[i] < a || carr[i] > b){
+            carr.splice(i, 1)
+        }
+    }
+}
+
+let carr = [5, 3, 8, 1]
 filterRangeInPlace(arr, 1, 4)
 console.log(arr)
