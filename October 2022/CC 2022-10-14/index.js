daf23dadc257256f1c31cd5f73967b0c640c7316
Daniel <codingwdan@gmail.com>
2022-10-14 22:14:43 -0700

    Add coding challenge for October 14th, 2022

---
+++ October 2022/CC 2022-10-14/index.js
@@ -0,0 +1,28 @@
+// Write a function that when given a URL as a string, parses out just the domain name and returns it as a string. For example:
+
+// domainName("http://github.com/carbonfive/raygun") == "github" 
+// domainName("http://www.zombie-bites.com") == "zombie-bites"
+// domainName("https://www.cnet.com") == "cnet
+
+
+
+
+function domainName(website){
+  
+}
+
+domainName("http://github.com/carbonfive/raygun") //"github" 
+domainName("http://www.zombie-bites.com") //"zombie-bites"
+domainName("https://www.cnet.com") //"cnet"
+
+
+function domainName(url){
+    url = url.replace('http://', '')
+    url = url.replace('https://', '')
+    url = url.replace('www.', '')
+     
+     return url.split('.')[0]
+     }
+
+   //basically, we can split the .com or .org or whatever at the very end with a split method at the '.', and then just return the domain name before that. But that still leaves us with http or https or www.
+   //we have to remove that with replace, which we can replace in the string with nothing.
