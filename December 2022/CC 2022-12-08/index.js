94c3ead8fe86b0bcc211f1e9e08bdb0aa4229430
Daniel <codingwdan@gmail.com>
2022-12-08 22:42:58 -0800

    Add coding challenge for December 8th, 2022

---
+++ December 2022/CC 2022-12-08/index.js
@@ -0,0 +1,13 @@
+//================
+//Spacify
+//================
+
+// Modify the spacify function so that it returns the given string with spaces inserted between each character.
+
+// spacify("hello world") // returns "h e l l o   w o r l d"
+
+function spacify(str) {
+    return str.split('').map(x=> x + ' ').join('').trim()
+  }
+  
+  spacify('hello world')
