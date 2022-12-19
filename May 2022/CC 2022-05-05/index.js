5aa27e6da89a090c8b618bdaafdcdadfb73043ba
Daniel <codingwdan@gmail.com>
2022-12-18 21:45:50 -0800

    Add coding challenge for December 18th, 2022

--- May 2022/CC 2022-05-05/index.js
+++ May 2022/CC 2022-05-05/index.js
@@ -25,16 +25,3 @@ function sumMul(n,m){
   
   
   
-  //=====================
-  //Filter Out the Geese
-  //======================
-  // Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
-  
-  // The geese are any strings in the following array, which is pre-populated in your solution:
-  
-  function gooseFilter (birds) {
-    var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
-    
-   return birds.filter(b => !geese.includes(b));
-  };
-  
