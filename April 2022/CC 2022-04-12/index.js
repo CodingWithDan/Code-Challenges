28a2a1f815d0dfdcda0b781d0ee0cd782eb622b6
Daniel <codingwdan@gmail.com>
2022-12-24 22:33:09 -0800

    Add coding challenge for December 24th, 2022

--- April 2022/CC 2022-04-12/index.js
+++ April 2022/CC 2022-04-12/index.js
@@ -1,23 +1,3 @@
-//================================
-//Area or Perimeter
-//================================
-//You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
-//If it is a square, return its area. If it is a rectangle, return its perimeter.
-//Note: for the purposes of this kata you will assume that it is a square if its length and width are equal, otherwise it is a rectangle.
-
-const areaOrPerimeter = function(l , w) {
-    // Return your answer
-    let perea = 0;
-    if (l === w){
-      perea = l * w
-      return perea
-    }
-    
-    else{
-      perea = (l * 2) + (w * 2)
-      return perea
-    }
-  };
 
   //===============================================
   //Find Maximum and Minimum Values of a List
