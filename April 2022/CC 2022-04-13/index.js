13f10f5a749a4bc678d38babda2ed4a3c9bd3ed2
Daniel <codingwdan@gmail.com>
2022-12-26 10:35:24 -0800

    Add coding challenge for December 26th, 2022

--- April 2022/CC 2022-04-13/index.js
+++ April 2022/CC 2022-04-13/index.js
@@ -23,23 +23,4 @@ function pipeFix(numbers){
   }
   
   
-  //=================================
-  //What is betwen
-  //==================================
-  //Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
-  
-  // For example:
-  
-  // a = 1
-  // b = 4
-  // --> [1, 2, 3, 4]
-  
-  function between(a, b) {
-    let output = [];
-    for (let i = a; i <= b; i++){
-      output.push(i)
-      
-    }
-    return output;
-  }
   
