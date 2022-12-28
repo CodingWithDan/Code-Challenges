b1a8f3ca6b77e5699ca2cb3ae999dda0bfc05692
Daniel <codingwdan@gmail.com>
2022-12-27 19:24:18 -0800

    Add coding challenge for December 27th, 2022

--- April 2022/CC 2022-04-14/index.js
+++ April 2022/CC 2022-04-14/index.js
@@ -9,26 +9,3 @@ function greet(name){
   }
   
   
-  //==============================================================================
-  //Exclamation marks series #1: Remove an exclamation mark from the end of string
-  //===============================================================================
-  // //Remove an exclamation mark from the end of a string. For a beginner kata, you can assume that the input data is always a string, no need to verify it.
-  // Examples
-  
-  // remove("Hi!") === "Hi"
-  // remove("Hi!!!") === "Hi!!"
-  // remove("!Hi") === "!Hi"
-  // remove("!Hi!") === "!Hi"
-  // remove("Hi! Hi!") === "Hi! Hi"
-  // remove("Hi") === "Hi"
-  
-  function remove (string) {
-    let stringArr = string.split('')
-    let x = stringArr[stringArr.length - 1];
-    if (x === '!'){
-      stringArr.pop(x)
-    }
-    
-    return stringArr.join('');
-  }
-    
