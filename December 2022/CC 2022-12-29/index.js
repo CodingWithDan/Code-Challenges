c29af51761a33cba95b6e57662c1c77ea69013c9
Daniel <codingwdan@gmail.com>
2022-12-29 23:18:30 -0800

    Add coding challenge for December 29th, 2022

---
+++ December 2022/CC 2022-12-29/index.js
@@ -0,0 +1,13 @@
+//==============================
+//Name Shuffler
+//==============================
+//Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+  }
+  
+  nameShuffler('john McClane')
