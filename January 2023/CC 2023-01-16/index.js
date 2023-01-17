7b0c641789ad6e84da1b7992f55ad43753b357a4
Daniel <codingwdan@gmail.com>
2023-01-16 22:43:07 -0800

    add coding challenge for January 16th, 2023

---
+++ January 2023/CC 2023-01-16/index.js
@@ -0,0 +1,17 @@
+//===============
+//Greet Me
+//===============
+
+// Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+
+// Example:
+
+// "riley" --> "Hello Riley!"
+// "JACK"  --> "Hello Jack!"
+
+var greet = function(name) {
+    let nam = name.toLowerCase()
+     
+     return `Hello ${nam[0].toUpperCase() + nam.slice(1)}!`
+   };
+   
