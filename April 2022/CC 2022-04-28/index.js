8cd925e165260e976cc038925ad1e798b02b109b
Daniel <codingwdan@gmail.com>
2023-01-29 18:41:13 -0800

    Add coding challenge for January 29th, 2023

--- April 2022/CC 2022-04-28/index.js
+++ April 2022/CC 2022-04-28/index.js
@@ -168,17 +168,3 @@ function sum(array) {
      return array.map(i => 0 - i);
   }
   
-  
-
-  
-  
-  //==============
-  //Reversed Words
-  //=============
-  
-  //Complete the solution so that it reverses all of the words within the string passed in. 
-  
-  function reverseWords(str){
-   return str.split(" ").reverse().join(" ")
-    }
-  
