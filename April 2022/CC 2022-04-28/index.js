9515ab2fb0a68e3f1b58829c7d4b9e15aed62426
Daniel <codingwdan@gmail.com>
2023-02-02 22:38:52 -0800

    Add coding challenge for February 2nd, 2023

--- April 2022/CC 2022-04-28/index.js
+++ April 2022/CC 2022-04-28/index.js
@@ -141,30 +141,4 @@ function sum(array) {
       return newString
   }
   
-  
-  //===============
-  //Invert values
-  //===============
-  // Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
-  
-  // invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
-  // invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
-  // invert([]) == []
-  
-  // You can assume that all values are integers. Do not mutate the input array/list.
-  
-  function invert(array) {
-    let newArr = [];
-    for (let i = 0; i < array.length; i++){
-      newArr.push(array[i] * -1)
-    }
-     return newArr;
-  }
-  
-  
-  OR
-  
-  function invert(array) {
-     return array.map(i => 0 - i);
-  }
-  
+ 
