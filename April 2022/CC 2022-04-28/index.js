b4daa9836316658f30d8e7175af821906c1bc52b
Daniel <codingwdan@gmail.com>
2023-02-08 22:41:58 -0800

    Add coding challenge for Febuary 8th, 2023

--- April 2022/CC 2022-04-28/index.js
+++ April 2022/CC 2022-04-28/index.js
@@ -64,17 +64,3 @@ function sum(array) {
     return (n < 0 || m < 0) ? 0 : n * m
   }
   
-  //=======================
-  //Jenny's secret message
-  //=======================
-  //Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake. Can you help her?
-  
-  function greet(name){
-    if(name === "Johnny")
-      return "Hello, my love!";
-    else {
-      return "Hello, " + name + "!";
-    }
-  }
-  
-  
