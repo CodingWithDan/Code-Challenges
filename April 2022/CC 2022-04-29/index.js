bcf6bc417b48a9f357656ae54dfbdc7897134f98
Daniel <codingwdan@gmail.com>
2023-02-15 22:48:58 -0800

    add coding challenge for februry 15t,2023

--- April 2022/CC 2022-04-29/index.js
+++ April 2022/CC 2022-04-29/index.js
@@ -273,28 +273,3 @@ const reverseSeq = n => {
   }
   
   
-  //=========================
-  //Multiple of index
-  //=========================
-  //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
-  
-  function multipleOfIndex(array) {
-    let newArr = [];
-    
-    for (let i = 0; i <= array.length; i++){
-      if (array[i] % i === 0){
-        newArr.push(array[i])
-      }
-       
-    }
-    return newArr
-   
-  }
-  
-  
-  OR
-  
-  function multipleOfIndex(array) {
-    return array.filter((num, i) => num % i === 0);
-  }
-  
