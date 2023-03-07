d73c6ab32c81d2ffdac51480b8414501af6cabcd
Daniel <codingwdan@gmail.com>
2023-03-06 22:15:59 -0800

    add coding challenge for march 6th 2023

--- April 2022/CC 2022-04-29/index.js
+++ April 2022/CC 2022-04-29/index.js
@@ -149,14 +149,7 @@ const reverseSeq = n => {
     }
   
   
-  //==================
-  //Can We Divide It?
-  //==================
-  //Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
   
-  function isDivideBy(number, a, b) {
-   return (number % a === 0 && number % b === 0) ? true : false 
-   }
   
 
   
