1bfa13aabd5c101814e2a4af7c51da2e03dd2acc
Daniel <codingwdan@gmail.com>
2023-04-09 19:52:19 -0700

    Add coding challenge for April 9th, 2023

--- April 2022/CC 2022-04-29/index.js
+++ April 2022/CC 2022-04-29/index.js
@@ -22,17 +22,3 @@ const reverseSeq = n => {
   return x.split(' ').join('')
   }
   
-  //=========================
-  //Get the mean of an array
-  //=========================
-  // It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
-  
-  // Return the average of the given array rounded down to its nearest integer.
-  
-  // The array will never be empty.
-  
-  function getAverage(marks){
-   return Math.floor((marks.reduce((agg, c) => agg + c, 0))/marks.length)
-  }
-  
-  
