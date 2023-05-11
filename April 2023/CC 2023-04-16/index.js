8e464fcff32cf43496e9a9fe511c0a2692b372fc
Daniel <codingwdan@gmail.com>
2023-05-10 22:14:08 -0700

    Add coding challenge for May 10th, 2023

--- April 2023/CC 2023-04-16/index.js
+++
@@ -1,9 +0,0 @@
-//====================
-//Sum of angles
-//====================
-
-// Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
-
-function angle(n) {
-    return (n - 2) * 180
-   }
