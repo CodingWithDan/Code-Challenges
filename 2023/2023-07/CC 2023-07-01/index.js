2c5ee5ef2e28d840ab7b0526f5c427fcc303ea57
Daniel <codingwdan@gmail.com>
2023-07-01 21:36:19 -0700

    Add coding challenge for July 1st, 2023

---
+++ 2023/2023-07/CC 2023-07-01/index.js
@@ -0,0 +1,82 @@
+//=====================================
+//LC. 217. Contains Duplicate
+//=====================================
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+ 
+
+// Constraints:
+
+//     1 <= nums.length <= 105
+//     -109 <= nums[i] <= 109
+
+function containsDuplicate(nums) {
+
+    let numsSort = nums.sort((a,b) => a - b)
+
+    for (let i = 0; i < numsSort.length; i++){
+        if(numsSort[i] == numsSort[i + 1]){
+            return true
+        }
+        
+    }
+    return false
+    }
+
+containsDuplicate(([1,2,3,1]), true)
+containsDuplicate(([1,2,3,4]), false)
+containsDuplicate(([2,14,18,22,22]), true)
+//MY ALT
+
+function containsDuplicate(nums) {
+
+    for (let i = 0; i < nums.length; i++){
+        if(nums.indexOf(nums[i]) !== nums.lastIndexOf(nums[i])){
+            return true
+        }
+        
+    }
+    return false
+    }
+
+containsDuplicate(([1,2,3,1]), true)
+containsDuplicate(([1,2,3,4]), false)
+containsDuplicate(([2,14,18,22,22]), true)
+
+
+//LEON ALT
+
+function containsDupes(arr){
+    let numsNap = {}
+    for (let i = 0; i < arr.length; i++){
+        let num = arr[i]
+    if(numsMap[num]){
+        //if the number already exists in the object, end the loop and return true
+        return true
+    }
+    else{
+        numsMap[num] = true
+        //if it wasn't in the object, add the number to it
+    }
+
+    }
+
+    return false
+}
