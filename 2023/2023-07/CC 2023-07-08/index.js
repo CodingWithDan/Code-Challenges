a32faffff55ea15eccb9b39a227fff6d886e73c1
Daniel <codingwdan@gmail.com>
2023-07-08 17:45:20 -0700

    Add coding challenge for July 8th, 2023

---
+++ 2023/2023-07/CC 2023-07-08/index.js
@@ -0,0 +1,53 @@
+//====================================================
+//2710. Remove Trailing Zeros From a String
+//====================================================
+// Given a positive integer num represented as a string, return the integer num without trailing zeros as a string.
+
+ 
+
+// Example 1:
+
+// Input: num = "51230100"
+// Output: "512301"
+// Explanation: Integer "51230100" has 2 trailing zeros, we remove them and return integer "512301".
+
+// Example 2:
+
+// Input: num = "123"
+// Output: "123"
+// Explanation: Integer "123" has no trailing zeros, we return integer "123".
+
+ 
+
+// Constraints:
+
+//     1 <= num.length <= 1000
+//     num consists of only digits.
+//     num doesn't have any leading zeros.
+
+
+var removeTrailingZeros = function(num) {
+    let newNums = num.split('')
+      for (let i = newNums.length - 1; i > 0; i--){
+          if(newNums[i] === '0'){
+             newNums.pop()
+          }
+          else{
+              break
+          }
+      }
+        return newNums.join('')
+    };
+    
+    console.log(removeTrailingZeros("51230100"))
+    console.log(removeTrailingZeros("123"))
+    
+    
+    //ALT
+    
+    function removeTrailingZeros(num){
+        if(num.split('').lastIndexOf('0')===num.length-1){
+            return removeTrailingZeros(num.split('').splice(0,num.length-1).join(''));
+        }
+        return num;
+    };
