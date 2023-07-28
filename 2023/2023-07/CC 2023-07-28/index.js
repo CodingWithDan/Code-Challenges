f77111d8d8421cbe0a7429db8d940248315d3a16
Daniel <codingwdan@gmail.com>
2023-07-28 14:49:36 -0700

    Add coding challenge for July 28th, 2023

---
+++ 2023/2023-07/CC 2023-07-28/index.js
@@ -0,0 +1,61 @@
+//=========================================
+//LC. 151. Reverse Words in a String
+//=========================================
+
+// Given an input string s, reverse the order of the words.
+
+// A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+// Return a string of the words in reverse order concatenated by a single space.
+
+// Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+ 
+
+// Example 1:
+
+// Input: s = "the sky is blue"
+// Output: "blue is sky the"
+
+// Example 2:
+
+// Input: s = "  hello world  "
+// Output: "world hello"
+// Explanation: Your reversed string should not contain leading or trailing spaces.
+
+// Example 3:
+
+// Input: s = "a good   example"
+// Output: "example good a"
+// Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+ 
+
+// Constraints:
+
+//     1 <= s.length <= 104
+//     s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+//     There is at least one word in s.
+
+
+var reverseWords = function(s) {
+    let ss = s.trim().split(' ').reverse()
+    let newArr = []
+    for (let i = 0; i < ss.length; i++){
+        if(ss[i].length > 0){
+            newArr.push(ss[i])
+        }
+    }
+    return newArr.join(' ')
+};
+
+reverseWords("a good   example")
+
+//ALT
+
+
+var reverseWords = function(s) {
+    return s.trim().split(' ').reverse().filter(item => item !== '').join(' ')
+};
+
+reverseWords("a good   example")
