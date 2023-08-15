ebc7a76fcc34d5c6ea5f0f84fa2b083359218816
Daniel <codingwdan@gmail.com>
2023-08-14 19:34:19 -0700

    Add coding challenge for August 14th, 2023

--- 2023/2023-08/CC 2023-08-13/index.js
+++ 2023/2023-08/CC 2023-08-13/index.js
@@ -30,3 +30,15 @@ function dropCap(m) {
     
     return newArr.join(' ')
   }
+
+  //ALT
+
+  function dropCap(n) {
+    n = n.split(' ');
+    for(var i = 0; i < n.length; i++) {
+      if(n[i].length > 2) {
+        n[i] = n[i].slice(0, 1).toUpperCase() + n[i].slice(1).toLowerCase();
+      }
+    }
+    return n.join(' ');
+  }
