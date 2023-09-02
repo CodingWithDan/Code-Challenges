b97844b7bfac8f1240c94512f90dcb2279313274
Daniel <codingwdan@gmail.com>
2023-09-01 20:47:03 -0700

    Add coding challenge for September 1st, 2023

---
+++ 2023/2023-09/CC 2023-09-01/index.ts
@@ -0,0 +1,20 @@
+
+//======================
+//CW: Opposite number
+//======================
+
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+export class Kata {
+    static opposite(n: number) {
+      return n * -1
+    }
+  }
+  
+  
