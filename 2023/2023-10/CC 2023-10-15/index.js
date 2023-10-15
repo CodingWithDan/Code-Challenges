3584a62b7a96ed673f67066c067efbf28eee6965
Daniel <codingwdan@gmail.com>
2023-10-15 11:13:50 -0700

    Add coding challenge for October 15th, 2023

---
+++ 2023/2023-10/CC 2023-10-15/index.js
@@ -0,0 +1,34 @@
+
+//==========================================
+//CW: Return a string's even characters.
+//==========================================
+
+// Write a function that returns a sequence (index begins with 1) of all the even characters from a string. If the string is smaller than two characters or longer than 100 characters, the function should return "invalid string".
+
+// For example:
+
+// "abcdefghijklm" --> ["b", "d", "f", "h", "j", "l"]
+// "a"             --> "invalid string"
+
+function evenChars(string) {
+    if (100 < string.length || string.length < 2){
+      return "invalid string"
+    }
+    let arr = []
+    for (let i = 1; i < string.length; i++){
+      if (i % 2 !== 0){
+        arr.push(string[i])
+      }
+    }
+    
+    return arr
+  }
+  
+  //alt
+  
+  function evenChars(string) {
+    return (string.length < 2 || string.length > 100) ? "invalid string" : 
+    [...string].filter((x, i) => i % 2);
+  }
+  
+  
