f0b8b0317e53d0b98869fe3407872d1b40de333c
Daniel <codingwdan@gmail.com>
2023-11-25 14:11:01 -0800

    Add coding challenge for November 25th, 2023

---
+++ 2023/2023-11/CC 2023-11-25/index.js
@@ -0,0 +1,21 @@
+
+//=======================
+//CW: esreveR
+
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
+
+
+reverse = function(array) {
+    let list = []
+    
+    for(let i = array.length - 1; i >= 0; i--){
+      list.push(array[i])
+    }
+    return list
+  }
+  
+  
+  
+  
