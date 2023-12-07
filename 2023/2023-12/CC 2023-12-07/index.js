efbdf3f0e80e7594b846d1e66a313c92d62a35f2
Daniel <codingwdan@gmail.com>
2023-12-07 15:33:53 -0800

    Add coding challenge for December 7th, 2023

---
+++ 2023/2023-12/CC 2023-12-07/index.js
@@ -0,0 +1,19 @@
+//=======================
+//CW: Array Deep Count
+//=======================
+
+// You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+// Examples
+
+// []                   -->  0
+// [1, 2, 3]            -->  3
+// ["x", "y", ["z"]]    -->  4
+// [1, 2, [3, 4, [5]]]  -->  7
+
+// The input will always be an array.
+
+
+function deepCount(a){
+    return a.reduce((acc, el) => Array.isArray(el) ? acc += deepCount(el) : acc, a.length)
+  }
+  
