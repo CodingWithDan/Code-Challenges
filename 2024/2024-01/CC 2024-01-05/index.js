96924f6f6a36161fa5900d583eec5f9dc59ddf1e
Daniel <codingwdan@gmail.com>
2024-01-05 17:34:54 -0800

    Add coding challenge for Jnuary 5th, 2024

---
+++ 2024/2024-01/CC 2024-01-05/index.js
@@ -0,0 +1,36 @@
+//------------------------------------------------
+// OBJECTS LOST GALAXY
+//------------------------------------------------
+//Create a dog object that has four properties and three methods
+
+let dog = {
+  breed: 'lab',
+  sex: 'female',
+  hairLength: 'long',
+  houseTrained: 'no',
+  
+  rollOver: function(){
+    console.log("Good poochie!")
+  },
+
+  pottyInside: function(){
+    console.log("Oh God why")
+  }
+
+}
+
+let cat = {}
+
+cat.breed = "ragdoll"
+cat.color = "tabby"
+cat.legs = 4
+cat.name = "Stella"
+cat.meow = function(){
+  console.log("MEOW")
+}
+cat.jump = function(){
+  console.log('BLAST OFF')
+}
+cat.nip = function(){
+  console.log('whoa...dude...')
+}
