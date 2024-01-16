06f9c0f0760aa230b3a4f503a12b68f053e26848
Daniel <codingwdan@gmail.com>
2024-01-15 18:37:55 -0800

    Add coding challenge for January 15th, 2024

---
+++ 2024/2024-01/CC 2024-01-15/index.js
@@ -0,0 +1,9 @@
+//=======================
+//Nth Root of a Number
+//=======================
+//Given two numbers x and n, calculate the (positive) nth root of x; this means that being r = result, r^n = x
+
+function root(x, n) {
+    return Math.pow(x, 1/n)
+   }
+   
