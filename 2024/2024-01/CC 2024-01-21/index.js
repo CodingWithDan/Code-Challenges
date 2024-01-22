3dd93c5aa04d00f12e4ca51d4b8ff88910a18889
Daniel <codingwdan@gmail.com>
2024-01-21 20:47:36 -0800

    Add coding challenge for January 21st, 2024

---
+++ 2024/2024-01/CC 2024-01-21/index.js
@@ -0,0 +1,16 @@
+
+//====================================
+//CW: Incorrect division method
+//====================================
+
+//This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+function solve(x,y){
+    return x/y
+  }
+  
+  //ALT
+  
+  const solve = (x, y) => x / y
+  
+  
